682a269eaa8f757e0e1bebb329b658137d46a237
Adailtondosantos12 <adailtonsanto10@hotmail.com>
2025-11-09 22:53:20 -0300

    Update welcome message in server response

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 BetRadar App rodando com sucesso!");
+  res.send("🚀 BetRadar Brasil rodando com sucesso!");
 });
 
 const port = process.env.PORT || 3000;
